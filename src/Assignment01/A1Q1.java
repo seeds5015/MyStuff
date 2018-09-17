a604619f80b9c25b272a58f0d61e1994aeab7b70
seeds5015 <seeds5015@HRH-IC0027318.wrdsb.ca>
2018-09-17 12:33:50 -0400

    First commit

---
+++ src/Assignment01/A1Q1.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment01;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author seeds5015
+ */
+public class A1Q1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+System.out.println("please eneter two integers on seperate lines to run calclations ");
+Scanner input = new Scanner (System.in);
+String users1Number = input.nextLine();
+String users2Number = input.nextLine();
+
+double 
+int birthYear = 2018 - usersAge;
+
+System.out.println("The sum of " + users1Number + users2Number);
+
+System.out.println( users1Number );    
+
+
+
+//System.out.println("5/2 is " + ans);
+    
+    
+    }
+}
