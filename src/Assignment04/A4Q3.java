22fbaf7fdb7bbed62dab4ee7a3ec009321c7d5a5
seeds5015 <seeds5015@HRH-IC0027318.wrdsb.ca>
2018-10-25 14:22:54 -0400

    Assignment03

--- src/Assignment04/A4Q3.java
+++ src/Assignment04/A4Q3.java
@@ -19,9 +19,24 @@ public class A4Q3 {
         // TODO code application logic here
    
     Scanner input = new Scanner(System.in);
-    String word = input.nextLine();
     
     
+        System.out.println("Please enter a word");
+        String word = input.nextLine();
+        
+        
+        if(word.length ()==1){
+            System.out.println("The word with the last characters moved removed is " + word);
+        // -2 means it will highlight the last two letters
+        }String newWord1 = word.substring(word.length()-2);
+        
+       // 0,-2 means it will highlight everthing but the last two letters
+        String newWord2 = word.substring(0, word.length()-2);
+        //places the last two letter infront the other highlighted lettering
+        String newWord = newWord1+newWord2;
+        System.out.println("The word with the last characters moved removed is " + newWord );
+                
+             
     
     }
 }
