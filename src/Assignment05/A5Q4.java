32cdd0b68dd7e8c73e0a47b4b41773c12fee87e7
seeds5015 <seeds5015@HRH-IC0027318.wrdsb.ca>
2018-12-06 14:24:54 -0500

    Assignment03

--- src/Assignment05/A5Q4.java
+++ src/Assignment05/A5Q4.java
@@ -18,42 +18,95 @@ public class A5Q4 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-  Scanner input = new Scanner(System.in);
-        System.out.println("type in your 10 numbers");
-    int[] grades = new int[10];
-    
-   
-        for (int i = 0; i < 10; i++) {
-              
-         grades [i] = input.nextInt();
-         
-         
+  
         
-        }for (int i = 0; i < 10; i++) {
+        
+  int n, grade;      
+        Scanner s = new Scanner(System.in);
+ 
+  
+  
+  System.out.println("Enter number of marks");
+  
+  
+  n = s.nextInt();
+  
+  int a [] = new int[10];
+  
+        System.out.println("Enter all marks");
+  
+        for (int i = 0; i <  n; i++) 
+            {  
+                a[i]=s.nextInt();
+            }
+            for (int i = 0; i < n; i++) 
+                {
+        
+          
+            for (int j = i + 1; j < n; j++)
+            {
+            if (a[i] >a[j])
+            {
+                
+            grade = a[i];
+            a[i]=a[j];
+            a[j]= grade;
+        }
+        }
+    
+                }
             
-                    
-               
-                           
-                
-                
-            }       
-         for (int i = 0; i < grades.length -1; i++) {
-         if (grades[0] <  grades[i = -1]) {
-            int storage  = grades[i];
-           grades[i] = grades[i=-1];    
-           grades[i=-1]=storage;
-           
-              
-//        }
-         
-         }
-        }    
-        
+for (int i = 0; i < n -1; i++)
+{
+    System.out.println(a[i] + " , ");
     
-    for (int i = 0; i < grades.length ; i++){
-        System.out.println(grades[i]);
-}
     }
+System.out.println(a[n -1]);
+    
+    }
+}
+
+
+
+ 
+  
+
+  
+  
+  
+//    int[] grades = new int[5];
+//    
+//   
+//        for (int i = 0; i < 5; i++) {
+//              
+//         grades [i] = input.nextInt();
+//         
+//    int storage  = grades[i];     
+//        
+//        }for (int i = 0; i < 5; i++) {
+//    grades[i] = input.nextInt();        
+//                    int storage  = grades[i];
+//               
+//                           
+//                
+//                
+//            }       
+//         for (int i = 0; i < grades.length -1; i++) {
+//         if (grades[0] <  grades[i = -1]) {
+//            int storage  = grades[i];
+//           grades[i] = grades[i=-1];    
+//           grades[i=-1]=storage;
+//           
+//              
+////        }
+//         
+//         }
+//        }    
+//        
+//    
+//    for (int i = 0; i < grades.length ; i++){
+//        System.out.println(grades[i=-1]);
+//}
+    
         
 
-} 
